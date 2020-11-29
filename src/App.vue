80df51db1a66f9165c1ff58d7d6f42fb597c171d
Camrado <blackandwhite.b17w@gmail.com>
2020-11-29 22:01:24 +0400

    Added navbar and pages (with nothing)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <router-view />
+  <div class="navbar">
+    <div class="navbar__wrapper wrapper">
+      <router-link to="/" class="navbar__logo">
+        Wibako
+      </router-link>
+      <div class="navbar__links">
+        <router-link to="/posts" class="navbar__links_link">Posts</router-link>
+        <router-link to="/about" class="navbar__links_link">About</router-link>
+      </div>
+    </div>
+  </div>
+
+  <div class="wrapper">
+    <router-view />
+  </div>
 </template>
 
 <script>
@@ -8,4 +22,44 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#app {
+  font-family: "Poppins", sans-serif;
+}
+
+.wrapper {
+  width: 1100px;
+  margin: auto;
+}
+
+.navbar {
+  width: 100%;
+  height: 70px;
+  background-color: $dark-bg;
+
+  &__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+  }
+
+  &__logo {
+    font-size: 35px;
+    font-family: "Lobster Two", cursive;
+    color: $text-on-dark;
+  }
+
+  &__links {
+    display: flex;
+    justify-content: space-between;
+    width: 25%;
+
+    &_link {
+      color: $text-on-dark;
+      font-size: 24px;
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
+import Posts from "@/views/Posts.vue";
+import About from "@/views/About.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Wibako - Home page",
+    },
+  },
+  {
+    path: "/posts",
+    name: "Posts",
+    component: Posts,
+    meta: {
+      title: "Wibako - Posts",
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    meta: {
+      title: "Wibako - About",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to) => {
+  document.title = to.meta.title;
 });
 
 export default router;
