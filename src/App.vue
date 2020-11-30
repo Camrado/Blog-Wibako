689c85e4e2d41b9ca313562f13e867ca8fb5933f
Camrado <blackandwhite.b17w@gmail.com>
2020-11-30 21:55:31 +0400

    Created home page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar">
+  <nav class="navbar">
     <div class="navbar__wrapper wrapper">
       <router-link to="/" class="navbar__logo">
         Wibako
@@ -9,33 +9,98 @@
         <router-link to="/about" class="navbar__links_link">About</router-link>
       </div>
     </div>
-  </div>
+  </nav>
 
-  <div class="wrapper">
-    <router-view />
-  </div>
+  <router-view />
+
+  <footer class="footer">
+    <div class="footer__content wrapper">
+      <div class="footer__info">
+        <div class="footer__info_logo">
+          <img src="@/assets/light-logo.png" alt="Logo" class="footer__info_img" />
+          <p class="footer__info_text">Wibako</p>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo exercitationem cupiditate quasi vel debitis recusandae ipsum aspernatur nisi
+          facere? Ut?
+        </p>
+      </div>
+
+      <div class="footer__support">
+        <h3 class="footer__support_title">
+          Support
+        </h3>
+        <ul class="footer__support_list">
+          <li>Contact us</li>
+          <li>FAQ</li>
+          <li>Downloads</li>
+          <li>Locate a dealer</li>
+          <li>Product registration</li>
+          <li>Spare parts</li>
+        </ul>
+      </div>
+
+      <div class="footer__wibako">
+        <h3 class="footer__wibako_title">
+          Wibako
+        </h3>
+        <ul class="footer__wibako_list">
+          <li>About Wibako</li>
+          <li>Wibako design</li>
+          <li>Careers</li>
+          <li>Newsroom</li>
+          <li>Wibako access</li>
+        </ul>
+      </div>
+
+      <div class="footer__letter">
+        <h3 class="footer__letter_title">
+          Stay up to date on the latest from Wibako
+        </h3>
+        <input type="text" class="footer__letter_input" placeholder="Enter your e-mail address" />
+        <button class="footer__letter_btn">Sign up</button>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
+import { onMounted } from "vue";
+
 export default {
   name: "App",
+
+  setup() {
+    onMounted(() => {
+      window.addEventListener("scroll", changeBgOfNav);
+    });
+
+    function changeBgOfNav() {
+      let navbar = document.querySelector(".navbar");
+      navbar.classList.toggle("changebg", window.scrollY > 0);
+    }
+
+    return {};
+  },
 };
 </script>
 
 <style lang="scss">
 #app {
   font-family: "Poppins", sans-serif;
+  background: $dark-bg;
 }
 
-.wrapper {
-  width: 1100px;
-  margin: auto;
+.changebg {
+  background-color: $navbar-bg;
 }
 
 .navbar {
+  position: fixed;
   width: 100%;
   height: 70px;
-  background-color: $dark-bg;
+  z-index: 100;
+  transition: 0.65s;
 
   &__wrapper {
     display: flex;
@@ -47,7 +112,7 @@ export default {
   &__logo {
     font-size: 35px;
     font-family: "Lobster Two", cursive;
-    color: $text-on-dark;
+    color: $text-color;
   }
 
   &__links {
@@ -56,9 +121,90 @@ export default {
     width: 25%;
 
     &_link {
-      color: $text-on-dark;
+      color: $text-color;
       font-size: 24px;
       text-transform: uppercase;
+    }
+  }
+}
+
+.footer {
+  margin-top: 70px;
+  background: $navbar-bg;
+  // border-top: 1px solid $text-color;
+  padding: 70px 0;
+
+  &__content {
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr 2fr;
+    grid-column-gap: 40px;
+  }
+
+  &__info {
+    &_logo {
+      display: flex;
+      align-items: center;
+      margin-bottom: 15px;
+      cursor: pointer;
+    }
+    &_text {
+      font-size: 35px;
+      font-family: "Lobster Two", cursive;
+      margin-left: 13px;
+    }
+    &_img {
+      width: 40px;
+    }
+  }
+
+  &__support,
+  &__wibako {
+    &_title {
+      font-size: 30px;
+      margin-bottom: 15px;
+      cursor: pointer;
+    }
+    &_list {
+      list-style: none;
+
+      li {
+        cursor: pointer;
+      }
+
+      @for $i from 2 through 6 {
+        li:nth-child(#{$i}) {
+          padding: 3.5px 0;
+        }
+      }
+    }
+  }
+
+  &__letter {
+    display: flex;
+    flex-direction: column;
+
+    &_title {
+      font-size: 20px;
+    }
+    &_input {
+      margin-top: 30px;
+      border: none;
+      background: none;
+      padding: 10px;
+      outline: none;
+      font-size: 18px;
+      color: $text-color;
+      border-bottom: 1px solid $text-color;
+    }
+    &_btn {
+      margin-top: 30px;
+      outline: none;
+      border: none;
+      background: $text-color;
+      color: $dark-bg;
+      padding: 15px;
+      font-size: 18px;
+      width: 180px;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,78 @@
 <template>
-  <div class="home">
-    This is a home page.
+  <div class="home-page">
+    <section class="header-sec">
+      <div class="header-sec__text">
+        <h1>
+          Welcome to
+          <span class="header-sec__text_wibako">Wibako</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tempore distinctio nemo dolor pariatur? Adipisci, asperiores rem id
+          suscipit eos debitis. Necessitatibus saepe fugiat id iusto dolorum, atque ab consectetur.
+        </p>
+      </div>
+    </section>
+
+    <section class="information">
+      <div class="wrapper">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <div class="information__content">
+          <div class="information__content_text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae expedita error quas laudantium mollitia, similique cum voluptas?
+              Perferendis, voluptatibus temporibus? Repellat beatae aliquam quaerat? Exercitationem veritatis autem unde quae sit vel, earum obcaecati
+              enim laborum? Laborum eum consequuntur repellendus dolores nobis reiciendis, amet impedit error at, tempore quasi a modi.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, quo ab nihil mollitia accusamus dolore recusandae sapiente sunt,
+              pariatur aliquid voluptate ducimus in soluta ullam tempora minima! Reiciendis vero tempore numquam debitis? Explicabo laborum
+              perferendis, dolores ab asperiores dignissimos labore consectetur quasi quisquam maxime fugit.
+            </p>
+          </div>
+          <img src="../assets/homepage/blog-post.svg" alt="Blog post" class="information__content_img" />
+        </div>
+      </div>
+    </section>
+
+    <section class="information">
+      <div class="wrapper">
+        <div class="information__content">
+          <div class="information__content_text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae expedita error quas laudantium mollitia, similique cum voluptas?
+              Perferendis, voluptatibus temporibus? Repellat beatae aliquam quaerat? Exercitationem veritatis autem unde quae sit vel, earum obcaecati
+              enim laborum? Laborum eum consequuntur repellendus dolores nobis reiciendis, amet impedit error at, tempore quasi a modi.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, quo ab nihil mollitia accusamus dolore recusandae sapiente sunt,
+              pariatur aliquid voluptate ducimus in soluta ullam tempora minima! Reiciendis vero tempore numquam debitis? Explicabo laborum
+              perferendis, dolores ab asperiores dignissimos labore consectetur quasi quisquam maxime fugit.
+            </p>
+          </div>
+          <img src="../assets/homepage/blog-post-2.svg" alt="Blog post" class="information__content_img" />
+        </div>
+      </div>
+    </section>
+
+    <section class="information">
+      <div class="wrapper">
+        <div class="information__content">
+          <div class="information__content_text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae expedita error quas laudantium mollitia, similique cum voluptas?
+              Perferendis, voluptatibus temporibus? Repellat beatae aliquam quaerat? Exercitationem veritatis autem unde quae sit vel, earum obcaecati
+              enim laborum? Laborum eum consequuntur repellendus dolores nobis reiciendis, amet impedit error at, tempore quasi a modi.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, quo ab nihil mollitia accusamus dolore recusandae sapiente sunt,
+              pariatur aliquid voluptate ducimus in soluta ullam tempora minima! Reiciendis vero tempore numquam debitis? Explicabo laborum
+              perferendis, dolores ab asperiores dignissimos labore consectetur quasi quisquam maxime fugit.
+            </p>
+          </div>
+          <img src="../assets/homepage/blog-post-3.svg" alt="Blog post" class="information__content_img" />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -9,3 +81,76 @@ export default {
   name: "Home",
 };
 </script>
+
+<style lang="scss" scoped>
+.home-page {
+  background: $dark-bg;
+}
+
+.header-sec {
+  position: relative;
+  background-image: url("../assets/homepage/blog-bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-y: -150px;
+  height: 100vh;
+  max-width: 100%;
+
+  &__text {
+    position: absolute;
+    top: 30%;
+    left: 5%;
+    max-width: 30%;
+
+    &_wibako {
+      border-bottom: 2px solid $text-color;
+    }
+
+    h1 {
+      font-size: 40px;
+      margin-bottom: 25px;
+    }
+  }
+}
+
+.information {
+  min-height: 70vh;
+  padding-top: 100px;
+
+  &__content {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    &_text {
+      width: 55%;
+    }
+
+    &_img {
+      width: 35%;
+      user-select: none;
+    }
+  }
+
+  h1 {
+    font-size: 40px;
+    margin-bottom: 50px;
+  }
+
+  p {
+    text-indent: 65px;
+    margin-bottom: 20px;
+  }
+
+  &:nth-child(odd) {
+    .information__content_text {
+      order: 2;
+    }
+    .information__content_img {
+      order: 1;
+    }
+  }
+}
+</style>
